3b44c7851a54d09561548b994c953d2ddbcd56b9
Mael <mael7ax@gmail.com>
2025-08-26 21:00:33 +0600

    dz5

--- js/lesson.js
+++ js/lesson.js
@@ -82,3 +82,50 @@ function getPosts() {
     })
 }
 getPosts()
+
+// converter
+const somInput = document.querySelector("#som");
+const usdInput = document.querySelector("#usd");
+const eurInput = document.querySelector("#eur");
+
+const convertor = (element) => {
+    element.addEventListener("input", async () => {
+        try {
+            const response = await fetch('../data/converter.json');
+
+            if (!response.ok) {
+                throw new Error(`Ошибка: ${response.status}`);
+            }
+
+            const data = await response.json();
+
+            const som = parseFloat(somInput.value);
+            const usd = parseFloat(usdInput.value);
+            const eur = parseFloat(eurInput.value);
+
+            if (element.id === 'som') {
+                usdInput.value = (element.value / data.usd).toFixed(2);
+                eurInput.value = (element.value / data.eur).toFixed(2);
+            } else if (element.id === 'usd') {
+                somInput.value = (element.value * data.usd).toFixed(2);
+                eurInput.value = ((element.value * data.usd) / data.eur).toFixed(2);
+            } else if (element.id === 'eur') {
+                usdInput.value = ((element.value * data.eur) / data.usd).toFixed(2);
+                somInput.value = (element.value * data.eur).toFixed(2);
+            }
+
+            if (element.value === '') {
+                somInput.value = '';
+                usdInput.value = '';
+                eurInput.value = '';
+            }
+
+        } catch (error) {
+            console.error('Ошибка при конвертации валют:', error);
+        }
+    });
+};
+
+convertor(somInput);
+convertor(usdInput);
+convertor(eurInput);
